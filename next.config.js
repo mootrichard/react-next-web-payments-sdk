4817268a5de714aebf0f8055597bf09fb42f2ca4
Richard Moot <moot@squareup.com>
2022-03-07 12:07:26 -0800

    Initial commit for implementing react-square-web-payments-sdk in nextjs

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
+const withTM = require('next-transpile-modules')(['@square/web-sdk', 'react-square-web-payments-sdk'])
+
+module.exports = withTM({
   reactStrictMode: true,
-}
+  experimental: {
+    esmExternals: 'loose'
+  }
+})
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { AchPay, CreditCardInput, SquarePaymentsForm } from 'react-square-web-payments-sdk'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,44 +13,28 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <SquarePaymentsForm
+          applicationId='sandbox-sq0idb-B-i4MbEZEARTWqSj7Gs9Ww'
+          locationId='9PKBD3JCQFH0D'
+          cardTokenizeResponseReceived={ async (token, buyer) => {
+            const response = await fetch('/api/pay', {
+              method: 'POST',
+              headers: {
+                'Content-type': 'application/json'
+              },
+              body: JSON.stringify({
+                sourceId: token.token
+              })
+            })
+            alert(JSON.stringify(await response.json(), null , 2));
+          }}
+        >
+          <CreditCardInput />
+          <p></p>
+          <AchPay
+            accountHolderName='Richard Moot'
+          />
+        </SquarePaymentsForm>
       </main>
 
       <footer className={styles.footer}>
